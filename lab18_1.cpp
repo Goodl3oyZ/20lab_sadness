6d9db10e2d3dd94510bb7498661c501181f9300b
neokarn <34811101+neokarn@users.noreply.github.com>
2021-02-22 20:40:03 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,13 +5,8 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(Rect,Rect);
+double overlap(){
 
-int main(){
-	cout << "Please input Rect 1 (x y w h): ";
-	cout << "Please input Rect 2 (x y w h): ";
-	
-	
-	cout << "Overlap area = ";	
-	return 0;
+
+
 }
