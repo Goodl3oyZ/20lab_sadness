199fb5a10ceb25b86f52cb6fc2279577dd6734ab
Goodl3oyZ <panudetzx2@gmail.com>
2023-02-21 16:44:38 +0700

    -> copy paste

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -4,9 +4,22 @@ using namespace std;
 struct Rect{
 	double x,y,w,h;
 };
-
-double overlap(){
-
-
-
+double min(double x,double y)
+{
+    if(x>=y) return y;
+    else return x;
+}
+double max(double x,double y)
+{
+    if(x>=y) return x;
+    else return y;
+}
+double overlap(Rect *A,Rect *B){
+    double dx,dy;
+    dy=min(A->y,B->y)-max((A->y-A->h),(B->y-B->h));
+    dx=min((A->x+A->w),(B->x+B->w))-max(A->x,B->x);
+    
+    if(dx<=0 || dy<=0){
+        return 0;
+    }else return dx*dy;
 }
